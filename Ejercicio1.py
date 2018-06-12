c2c205748f62fc62d14e60144fb96105fa292987
msand14 <39996479+msand14@users.noreply.github.com>
2018-06-12 16:07:37 +0200

    Add files via upload
    Final version Ejercicio 1, Caso práctico, Unit1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -7,17 +7,52 @@
 import csv
 import json
 
-#def tweet_analysis( ):
-file = open("/home/mescalonilla/bin/pycharm-community-2018.1.3/Documentos/SalidaTweets.txt")
-file_reader=csv.reader(file)
-datos=[]
-
-# Reading each element of the file and appending it to  datos list
-for i,linea in enumerate(file_reader):
-    palabra = linea
-    datos.append(json.dumps(linea))
-
-
+# CONVIERTO LOS TWEETS EN UNA LISTA
+def tweets2list():
+    lista_tweets=[]
+    with open( "/home/mescalonilla/bin/pycharm-community-2018.1.3/Documentos/SalidaTweets.txt") as f:
+        for i,elemento in enumerate(f):
+            lista = json.loads(elemento)
+            if lista.get('text'):
+                lista_tweets.append(lista.get('text'))
+    return lista_tweets
 
 
+
+# CONVIERTO LOS SENTIMIENTOS DEL ARCHIVO, EN UN DICCIONARIO
+def feelings2dict():
+    rows = []
+    with open("/home/mescalonilla/bin/pycharm-community-2018.1.3/Documentos/Sentimientos.txt") as file:
+        for line in file:
+            line= line.replace('\n','')
+            if(line==''):
+                continue
+            rows.append(line.split('\t'))
+        #print(rows)
+        dic=dict(rows)
+        #print(dic)
+    return dic
+
+
+
+def printer( str, val):
+    print("EL SIGUIENTE TWEET:",str ," TIENE UN SENTIMIENTO ASOCIADO DE:",val,"\n")
+
+
+def feeling_of_tweet (lista_tweet, dic_feel):
+    for tweet in lista_tweet:
+        valor = 0
+        for sentimiento in dic_feel:
+            if sentimiento in tweet:
+                #print("\n","//////",sentimiento,"",int(dic_feel[sentimiento]),"\n")
+                valor = valor + int(dic_feel[sentimiento])
+        printer(tweet,valor)
+
+def main():
+    lista_tweets = tweets2list()
+    diccionario_sentimientos = feelings2dict()
+    feeling_of_tweet(lista_tweets,diccionario_sentimientos)
+
+
+main()
 
